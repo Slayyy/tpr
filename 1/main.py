a0b0ad923c8827bf4ed7a7f4f633ce9a4af4dc8e
Krystian Chmura <krystian.chmura@gmail.com>
2018-03-16 00:23:53 +0100

    1: done i think

--- 1/main.py
+++ 1/main.py
@@ -28,10 +28,11 @@ if __name__ == "__main__":
     }.iteritems():
 
         mode_results = {}
-        iteration_count = 5
+        iteration_count = 200
 
         mode_results["x"] = []
         mode_results["y"] = []
+        mode_results["delay"] = []
         mode_results["iteration_count"] = iteration_count
 
         for string_len in gen_lengths(max_len=(5 * 10**6)):
@@ -52,6 +53,7 @@ if __name__ == "__main__":
 
             m_bit_per_second = ((8 * payload_size * iteration_count)/(end_time-begin_time)) / 10**6
             mode_results["y"].append(m_bit_per_second)
+            mode_results["delay"].append(((10**3) * (end_time - begin_time))/iteration_count)
             
         results[send_mode_name] = mode_results
     if rank == 0:
--- 1/run.sh
+++ 1/run.sh
@@ -5,4 +5,4 @@ set -x
 mpiexec  -machinefile ./same_host -np 2 ./main.py | tee 1_shmem.json
 MPIR_CVAR_CH3_NOLOCAL=1 mpiexec  -machinefile ./same_host -np 2 ./main.py | tee 2_no_shmem.json
 MPIR_CVAR_CH3_NOLOCAL=1 mpiexec  -machinefile ./same_machine -np 2 ./main.py | tee 3_same_machine.json
-MPIR_CVAR_CH3_NOLOCAL=1 mpiexec  -machinefile ./diffrent_machines -np 2 ./main.py | tee 4_different_machines.json
+MPIR_CVAR_CH3_NOLOCAL=1 mpiexec  -machinefile ./different_machines -np 2 ./main.py | tee 4_different_machines.json
