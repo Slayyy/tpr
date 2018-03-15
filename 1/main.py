6824f1a6b39f438bc00f118e6b0387b1cfbaf300
Krystian Chmura <krystian.chmura@gmail.com>
2018-03-15 22:31:38 +0100

    1: without results

--- 1/main.py
+++ 1/main.py
@@ -1,8 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 import sys
-import pickle
+import gc
+import json
+import math
+import cPickle
 from mpi4py import MPI
 
+
+def gen_lengths(max_len):
+    x = 1
+    while x < max_len:
+        yield int(x)
+        if x < 50:
+            x += 1
+        else:
+            x *= 1.03
 
 if __name__ == "__main__":
     comm = MPI.COMM_WORLD
@@ -12,27 +24,35 @@ if __name__ == "__main__":
 
     for send_mode_name, send_function in {
             "send": lambda x: comm.send(x, dest=1),
-            "bsend": lambda x: comm.bsend(x, dest=1)
-    }.items():
+            "ssend": lambda x: comm.ssend(x, dest=1)
+    }.iteritems():
 
         mode_results = {}
-        iteration_count = 10000
+        iteration_count = 5
 
-        for string_len in range(1, 10):
+        mode_results["x"] = []
+        mode_results["y"] = []
+        mode_results["iteration_count"] = iteration_count
+
+        for string_len in gen_lengths(max_len=(5 * 10**6)):
             payload = "a" * string_len
-            payload_size = len(pickle.dumps(payload))
-            
+            payload_size = string_len + len(cPickle.dumps(""))
+
+            gc.collect()
             comm.Barrier()
             begin_time = MPI.Wtime()
-            for _ in range(iteration_count):
+            for _ in xrange(iteration_count):
                 if rank == 0:
                     send_function(payload)
                 elif rank == 1:
                     comm.recv(source=0)
-            comm.Barrier()
             end_time = MPI.Wtime()
-            mode_results[payload_size] = end_time-begin_time
+            comm.Barrier()
+            mode_results["x"].append(payload_size)
+
+            m_bit_per_second = ((8 * payload_size * iteration_count)/(end_time-begin_time)) / 10**6
+            mode_results["y"].append(m_bit_per_second)
             
         results[send_mode_name] = mode_results
-
-    print(results)
+    if rank == 0:
+        print json.dumps(results, sort_keys=True)
---
+++ 1/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -x
+
+mpiexec  -machinefile ./same_host -np 2 ./main.py | tee 1_shmem.json
+MPIR_CVAR_CH3_NOLOCAL=1 mpiexec  -machinefile ./same_host -np 2 ./main.py | tee 2_no_shmem.json
+MPIR_CVAR_CH3_NOLOCAL=1 mpiexec  -machinefile ./same_machine -np 2 ./main.py | tee 3_same_machine.json
+MPIR_CVAR_CH3_NOLOCAL=1 mpiexec  -machinefile ./diffrent_machines -np 2 ./main.py | tee 4_different_machines.json
